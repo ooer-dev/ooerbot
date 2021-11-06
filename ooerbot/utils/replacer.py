9ee98d1ab4985e7a0db6a8b73c54348b944d5c40
William McKinnerney <git@williammck.net>
2021-11-05 23:14:00 -0500

    Finish substitutions

--- ooerbot/utils/replacer.py
+++ ooerbot/utils/replacer.py
@@ -1,3 +1,4 @@
+import datetime
 import random
 import re
 
@@ -21,9 +22,9 @@ def replace_reaction_placeholders(bot: OoerBot, message: discord.Message, respon
         'bot.mention': bot.user.mention,
         'bot.name': bot.user.name,
         'bot.status': str(message.guild.me.status),
-        'bot.time': '',
+        'bot.time': datetime.datetime.now(datetime.timezone.utc).strftime('%H:%M UTC'),
 
-        'channel.created': '',
+        'channel.created': channel.created_at.strftime('%H:%M %d.%m.%Y'),
         'channel.id': str(channel.id),
         'channel.mention': channel.mention,
         'channel.name': channel.name,
@@ -33,17 +34,17 @@ def replace_reaction_placeholders(bot: OoerBot, message: discord.Message, respon
         'server.id': str(guild.id),
         'server.members': str(guild.member_count),
         'server.name': guild.name,
-        'server.time': '',
+        'server.time': datetime.datetime.now(datetime.timezone.utc).strftime('%H:%M UTC'),
 
         'user': member.mention,
         'user.avatar': str(member.avatar_url),
-        'user.created_date': '',
-        'user.created_time': '',
+        'user.created_date': member.created_at.strftime('%d.%m.%Y'),
+        'user.created_time': member.created_at.strftime('%H:%M'),
         'user.discrim': str(member.discriminator),
         'user.fullname': str(member),
         'user.id': str(member.id),
-        'user.joined_date': '',
-        'user.joined_time': '',
+        'user.joined_date': member.joined_at.strftime('%d.%m.%Y'),
+        'user.joined_time': member.joined_at.strftime('%H:%M'),
         'user.mention': member.mention,
         'user.name': member.name,
     }
@@ -53,7 +54,7 @@ def replace_reaction_placeholders(bot: OoerBot, message: discord.Message, respon
         key = match.group(1)
         return replacements.get(key, orig)
 
-    response = re.sub(r'%([a-z.]+)%', lookup, response)
+    response = re.sub(r'%([a-z._]+)%', lookup, response)
 
     def rng(match: re.Match) -> str:
         start = int(match.group('from') or 0)
